0d7ea0d15cdec5f88b794dc2e6b7d4ad487a16d8
Sam Nelson <sam.nelsonn@proton.me>
2023-01-23 20:17:02 +0100

    feat(nvim): automatically synchronize packages

--- nvim/lua/thenelsonn/packer.lua
+++ nvim/lua/thenelsonn/packer.lua
@@ -1,5 +1,13 @@
 vim.cmd("packadd packer.nvim")
 
+-- Automatically sync packages 
+vim.cmd [[
+    augroup packer_user_config
+        autocmd!
+        autocmd BufWritePost packer.lua source <afile> | PackerSync
+    augroup end
+]]
+
 return require("packer").startup(function(use)
     use("wbthomason/packer.nvim")       -- let packer.nvim manage itself
 end)
