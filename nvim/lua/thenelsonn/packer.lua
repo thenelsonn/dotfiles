b600fe48969cd8da785e8f02a3b9061d46c24dd4
Sam Nelson <sam.nelsonn@proton.me>
2023-01-25 20:14:15 +0100

    chore(nvim): change indent level

--- nvim/lua/thenelsonn/packer.lua
+++ nvim/lua/thenelsonn/packer.lua
@@ -1,13 +1,25 @@
 vim.cmd("packadd packer.nvim")
 
+local fn = vim.fn
+local pth = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(pth)) > 0 then
+  vim.notify("Packer can't be loaded, since it not installed on your system")
+  return
+end
+
 -- Automatically sync packages 
 vim.cmd [[
-    augroup packer_user_config
-        autocmd!
-        autocmd BufWritePost packer.lua source <afile> | PackerSync
-    augroup end
+  augroup packer_user_config
+  autocmd!
+  autocmd BufWritePost packer.lua source <afile> | PackerSync
+  augroup end
 ]]
 
-return require("packer").startup(function(use)
-    use("wbthomason/packer.nvim")       -- let packer.nvim manage itself
+local p_status, packer = pcall(require, "packer")
+if not p_status then
+  return
+end
+
+return packer.startup(function(use)
+  use("wbthomason/packer.nvim")       -- let packer.nvim manage itself
 end)
