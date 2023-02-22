3542a0484d13b051e91b179c08febf25b0d9586b
Sam Nelson <sam.nelsonn@proton.me>
2023-02-22 19:56:38 +0100

    feat(nvim): extend lsp list

--- nvim/lua/thenelsonn/lsp/null-ls.lua
+++ nvim/lua/thenelsonn/lsp/null-ls.lua
@@ -1,13 +1,17 @@
 local null_ls_status, null_ls = pcall(require, "null-ls")
 if not null_ls_status then
-  return
+	return
 end
 
 local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-  debug = false,
-  sources = {
-    formatting.stylua
-  }
+	debug = false,
+	sources = {
+		formatting.stylua,
+		formatting.prettier,
+		formatting.clang_format,
+		diagnostics.eslint,
+	},
 })
