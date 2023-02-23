32cb0d0369b8ac954c4a60d638540587b9f432e4
Sam Nelson <sam.nelsonn@proton.me>
2023-02-23 18:29:02 +0100

    fix(nvim): disable auto install

--- .config/nvim/after/plugin/treesitter.rc.lua
+++ .config/nvim/after/plugin/treesitter.rc.lua
@@ -8,7 +8,7 @@ local configs = require("nvim-treesitter.configs")
 configs.setup({
 	ensure_installed = {},
 	sync_install = false,
-	auto_install = true,
+	auto_install = false,
 	highlight = {
 		enable = true,
 		disable = {},
