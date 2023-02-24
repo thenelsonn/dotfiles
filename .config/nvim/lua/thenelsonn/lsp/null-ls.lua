3f65fc217f4c72aabd9f1e1f9daeb945eeac91c9
Sam Nelson <sam.nelsonn@proton.me>
2023-02-24 18:55:18 +0100

    fix(nvim): use spaces instead of tabs

--- .config/nvim/lua/thenelsonn/lsp/null-ls.lua
+++ .config/nvim/lua/thenelsonn/lsp/null-ls.lua
@@ -1,17 +1,17 @@
 local null_ls_status, null_ls = pcall(require, "null-ls")
 if not null_ls_status then
-	return
+  return
 end
 
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-	debug = false,
-	sources = {
-		formatting.stylua,
-		formatting.prettierd,
-		formatting.clang_format,
-		-- diagnostics.eslint,
-	},
+  debug = false,
+  sources = {
+    formatting.stylua.with({ extra_args = { "--indent-type Spaces", "--indent-width 2" } }),
+    formatting.prettierd,
+    formatting.clang_format,
+    -- diagnostics.eslint,
+  },
 })
